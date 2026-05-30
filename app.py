8b51a8b128ca15359b3b07c1700913c9d339efb2
Oluwatosin Oloruntade <Tosin.oloruntade97@gmail.com>
2026-05-30 21:50:37 +0100

    Tosin introduction

---
+++ app.py
@@ -0,0 +1,5 @@
+# Introudction to Team one
+
+# Hi, my name is Tosin 
+# One of my hobbies is fitness. I enjoy going for long walks and challenging myself
+# I enjoy it because it helps me stay active and achieve personal goals  
