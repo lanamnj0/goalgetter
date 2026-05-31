8d9ed3cf847414c94f0e08a79b462f86c4e4c096
Lana Manjouneh <lanaam127@gmail.com>
2026-05-31 16:09:48 +0100

    added intro - lana

--- app.py
+++ app.py
@@ -3,3 +3,8 @@
 # Hi, my name is Tosin 
 # One of my hobbies is fitness. I enjoy going for long walks and challenging myself
 # I enjoy it because it helps me stay active and achieve personal goals  
+
+
+# Hello, my name is Lana
+# I enjoy crochet and fitness. I love regularly going to the gym and
+# creating new healthy recipies, so that I can stay healthy and energised.  
