aeabde58d4d5b08f12ba66dd2e921bdad70fa0be
Lana Manjouneh <lanaam127@gmail.com>
2026-05-31 16:12:18 +0100

    added intro - lana

--- app.py
+++ app.py
@@ -4,7 +4,6 @@
 # One of my hobbies is fitness. I enjoy going for long walks and challenging myself
 # I enjoy it because it helps me stay active and achieve personal goals  
 
-
 # Hello, my name is Lana
 # I enjoy crochet and fitness. I love regularly going to the gym and
 # creating new healthy recipies, so that I can stay healthy and energised.  
