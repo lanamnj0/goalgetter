3c94bee39b8b18c3ad5b93e678e6f464f20c7817
Oluwatosin Oloruntade <Tosin.oloruntade97@gmail.com>
2026-06-14 23:09:22 +0100

    implement database connection setup for GoalGetter application

--- db_connection.py
+++ db_connection.py
@@ -4,6 +4,14 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="your_password", # replace with local password
+        password="your_password", # replace with MySQL password
         database="goalgetter"
     )
+
+if __name__ == "__main__":
+    connection = get_connection()
+
+    if connection.is_connected():
+        print("database successfully connected")
+
+    connection.close()
