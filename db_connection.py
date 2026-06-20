b0f4ac5e00d68fb2cd12214926f16f930a884e2d
Oluwatosin Oloruntade <Tosin.oloruntade97@gmail.com>
2026-06-20 17:49:24 +0100

    improve comments in code for readability

--- db_connection.py
+++ db_connection.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+# connect to the GoalGetter MySQL database using local credentials 
 def get_connection():
     return mysql.connector.connect(
         host="localhost",
@@ -14,4 +15,5 @@ if __name__ == "__main__":
     if connection.is_connected():
         print("database successfully connected")
 
+    # close the database connection after use 
     connection.close()
