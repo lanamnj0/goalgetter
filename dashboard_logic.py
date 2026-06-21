b109b169e6bf320912ac62c4a5e4629ba085372d
Neha Thiara <thiaraneha@gmail.com>
2026-06-21 23:33:53 +0100

    current UI architecture, some bugs prominent but will be quickly resolved

---
+++ dashboard_logic.py
@@ -0,0 +1,11 @@
+"""
+Functions to dynamically update
+(1)
+How many workouts have been completed and tally them up.
+
+(2)
+Streak of back-to-back sessions done.
+
+(3)
+Total workout time from all sessions.
+"""
--- frontend.py
+++ frontend.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, flash, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -19,8 +19,27 @@ def meals_and_recipes():
 def history():
     return render_template('history.html')
 
-@app.route('/support')
+@app.route('/support', methods=['GET', 'POST'])
 def support():
+    if request.method == 'POST' and 'name' in request.form and 'email' in request.form and 'message' in request.form and 'support_category' in request.form:  # issue type not included in this yet
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        selected_issue = request.form.get('support_category')
+
+        # These fields need to be saved into a support table within MySQL
+        # Also need case handling
+        counter = 0
+        support_tickets = []
+        if name and email and message:
+            for i in support_tickets:
+                counter += 1
+
+        # flash() entirely relies on browser Sessions and Cookies not MySQL
+        flash(f"Thank you! Your support ticket #{counter} has been submitted! A team member will email your shortly")
+        print(f"User selected: {selected_issue}")
+
+
     return render_template('support.html')
 
 @app.route('/settings')
