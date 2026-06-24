bcf04e7a4dfb0a887e9084364a660fe861e27177
Thelma <thelmatchitsa@gmail.com>
2026-06-24 22:12:07 +0100

    Functionality for creating/insert queries

--- db_utils.py
+++ db_utils.py
@@ -1,2 +1,12 @@
 from workout import run_select_queries, insert_data_queries, WorkoutExercises, Workouts
 
+def insert_workouts(table, data):
+    if table == "workouts":
+        query = """INSERT INTO workouts (user_id, workout_date, duration_minutes, calories_burned)
+        VALUES (%s, %s, %s, %s)""" 
+        insert_data_queries(query, data)
+    elif table == "workout_exercises":
+        query = """INSERT INTO workout_exercises (workout_id, exercise_id, set_count, reps, weight_kg) VALUES (%s, %s, %s, %s, %s)""" 
+        insert_data_queries(query, data)
+    
+    print("Executed")
