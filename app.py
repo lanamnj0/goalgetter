01fdc7e12ca0d7ff2b7838087fb84087ce4fe3ac
Neha Thiara <thiaraneha@gmail.com>
2026-06-24 15:48:03 +0100

    adding files for blueprinting - not finished yet

--- app.py
+++ app.py
@@ -1,27 +1,3 @@
-# Introduction to Team One
-
-# Hi, my name is Tosin 
-# One of my hobbies is fitness. I enjoy going for long walks and challenging myself
-# I enjoy it because it helps me stay active and achieve personal goals  
-
-# Hello, my name is Lana
-# I enjoy crochet and fitness. I love regularly going to the gym and
-# creating new healthy recipies, so that I can stay healthy and energised.
-
-# Hi, my name is Neha
-# One of my hobbies is road cycling 🚴‍♀️. Though it can be really, really hilly at times, and it can sometimes feel like a never-ending climb ⛰️😅 The
-# social chats, brief coffee stops, and lovely nature is a great way to break up city life. Really recommend giving it a go, I love it!
-
-# Heyy, my name is Shalesa! I enjoy travelling, I've just come back from Bali and went to Brazil for carnval last year. 
-# I also enjoy live music (festivals, concerts) 
-# I go to the gym twice a week and try to stick to a semi strict diet including tuna, eggs, salmon and chocolate (ha)
-
-# Hey hey, my name is Adeyosola 🌸
-# I love baking (favourite thing to bake are cookies) because it's really therapeutic for me and I love witnessing the process from start to finish.
-
-# Hi, Im Thelma 👋🏾
-# I enjoy cooking and trying new recipes from different cuisines, especially Asian cuisine 🍽. I also love going on walks and just being in nature 🌳.
-
 from config import Config
 from exercise_api import ExerciseAPI
 from flask import Flask, request, jsonify, render_template
