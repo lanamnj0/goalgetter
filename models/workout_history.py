6e60a5e678944b9e01083429b1e92e29ce859cdb
Shalesa Bridgette McAllister Iruaga <shalesa.bmi@outlook.com>
2026-06-25 18:30:43 +0100

    fix workout history indentation error

--- models/workout_history.py
+++ models/workout_history.py
@@ -6,21 +6,23 @@ class WorkoutNode:
 class WorkoutHistoryList:
     def __init__(self):
         self.head = None
-
-def add_workout(self, workout):
-    new_node = WorkoutNode(workout)
-
-    if self.head is None:
-        self.head = new_node
-        return
-    
-    current = self.head
-
-    while current.next:
-        current = current.next
         
-    current.next = new_node
-
+    def add_workout(self, workout):
+        
+        new_node = WorkoutNode(workout)
+        
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        current = self.head
+        
+        while current.next:
+            
+            current = current.next
+            
+            current.next = new_node
+            
     def get_workouts(self):
         workouts = []
         current = self.head
