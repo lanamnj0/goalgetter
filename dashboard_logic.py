370ba18b9ffdd3d43618de382dc4ae8bf8628736
Neha Thiara <thiaraneha@gmail.com>
2026-06-25 13:46:10 +0100

    working on calendar logic and unittesting

--- dashboard_logic.py
+++ dashboard_logic.py
@@ -9,3 +9,11 @@ Streak of back-to-back sessions done.
 (3)
 Total workout time from all sessions.
 """
+def calculate_total_workouts(history_list): # Trying to link to singly linked list
+    workouts = history_list.get_workouts()
+    total_workouts_count = len(workouts)
+    return total_workouts_count
+
+def calculate_current_streak(history_list):
+    workouts = history_list.get_workouts()
+    pass
