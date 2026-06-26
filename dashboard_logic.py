760a76fd762cbb65383e9103bba092f929a64ab9
Neha Thiara <thiaraneha@gmail.com>
2026-06-26 15:38:48 +0100

    merge conflict says too complex to resolve on github so i am pushing from ide to main to update file

--- dashboard_logic.py
+++ dashboard_logic.py
@@ -9,11 +9,42 @@ Streak of back-to-back sessions done.
 (3)
 Total workout time from all sessions.
 """
-def calculate_total_workouts(history_list): # Trying to link to singly linked list
-    workouts = history_list.get_workouts()
-    total_workouts_count = len(workouts)
-    return total_workouts_count
+from workout_history import *
+
+history_list = WorkoutHistoryList() # creating instance from Class WorkoutHistoryList
+
+def count_total_workouts(history_list):
+    if history_list is None:
+        return 0 # True so returning 0
+
+    workouts = history_list.get_workouts() # same function name as class method in WorkoutHistory
+
+    if workouts is None:
+        return 0
+
+    total_number_of_workouts_completed = len(workouts)
+    return total_number_of_workouts_completed
+
 
 def calculate_current_streak(history_list):
+    """
+    Streak function:
+    I'm leaving this as a placeholder for my teammate to help with.
+    """
+
+    return 0
+
+# workout.duration = one workout - how long it lasted
+def calculate_total_time_of_all_workouts(history_list):
+    if history_list is None:
+        return 0
+
     workouts = history_list.get_workouts()
-    pass
+    if workouts is None or len(workouts) == 0:
+        return 0
+
+    total_duration = 0
+    for workout in workouts:
+        total_duration += workout['duration']
+
+    return total_duration
